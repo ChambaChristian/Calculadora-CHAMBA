26b5c26cbccd5982966339f9511b8ff90776232a
CHRISTIAN ALEXANDER CHAMBA CLAVIJO <christian.chamba.clavijo@estudiantes.cordillera.edu.ec>
2021-06-13 16:15:04 -0500

    Agregar archivos de proyecto.

---
+++ Calculadora CHAMBA/MainActivity.cs
@@ -0,0 +1,118 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using Android.Support.V4.App;
+
+namespace Calculadora_CHAMBA
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            //DECLARACION DE VARIABLES
+            var resultado = FindViewById<TextView>(Resource.Id.resul);
+            var numero1 = FindViewById<EditText>(Resource.Id.num1);
+            var numero2 = FindViewById<EditText>(Resource.Id.num2);
+            var suma = FindViewById<Button>(Resource.Id.btnsumar);
+            var resta = FindViewById<Button>(Resource.Id.btnrestar);
+            var multi = FindViewById<Button>(Resource.Id.btnmulti);
+            var dividir = FindViewById<Button>(Resource.Id.btndiv);
+           
+            //var raiz = FindViewById<Button>(Resource.Id.btnraiz);
+
+
+            suma.Click += delegate
+           
+            {
+ 
+                if (numero1.Text == "" || numero2.Text =="")
+                {
+                    Toast.MakeText(this, "Falta ingresar información en el telefono", ToastLength.Short).Show();
+                }
+                else
+                {
+                    resultado.Text = (Convert.ToInt32(numero1.Text) + Convert.ToInt32(numero2.Text)).ToString();
+                    numero1.Text = "";
+                    numero2.Text = "";
+
+                }
+
+            };
+            resta.Click += delegate
+            {
+                if (numero1.Text == "" || numero2.Text == "")
+                {
+                    Toast.MakeText(this, "Falta ingresar información en el telefono", ToastLength.Short).Show();
+                }
+                else
+                {
+                    resultado.Text = (Convert.ToInt32(numero1.Text) - Convert.ToInt32(numero2.Text)).ToString();
+                    numero1.Text = "";
+                    numero2.Text = "";
+                }
+            };
+            multi.Click += delegate
+            {
+                if (numero1.Text == "" || numero2.Text == "")
+                {
+
+                }
+                else
+                {
+                    resultado.Text = (Convert.ToInt32(numero1.Text) * Convert.ToInt32(numero2.Text)).ToString();
+                    numero1.Text = "";
+                    numero2.Text = "";
+                }
+            };
+            dividir.Click += delegate
+            {
+                if(numero1.Text == "" || numero2.Text == "")
+                {
+
+                }
+                else
+                {
+                    resultado.Text = (Convert.ToInt32(numero1.Text) / Convert.ToInt32(numero2.Text)).ToString();
+                    numero1.Text = "";
+                    numero2.Text = "";
+                }
+            };
+            //potencia.Click += delegate
+            //{
+            //    if (numero1.Text == "" || numero2.Text == "")
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        resultado.Text = Math.Pow(Convert.ToInt32(numero1.Text), Convert.ToInt32(numero2.Text)).ToString();
+            //        numero1.Text = "";
+            //        numero2.Text = "";
+            //    }
+            //};
+            //raiz.Click += delegate
+            //{
+
+            //    resultado.Text = Math.Sqrt(Convert.ToInt32(numero1.Text), Convert.ToInt32(numero2.Text)).ToString();
+            //    numero1.Text = "";
+            //    numero2.Text = "";
+
+            //};
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Calculadora CHAMBA/Operaciones.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculadora_CHAMBA
+{
+    class Operaciones
+    {
+        //atributos y metodos
+        double numero1, resultado=0;
+
+        public Operaciones(double numero1, double resultado)
+        {
+            this.numero1 = numero1;
+            this.resultado = resultado;
+        }
+        public void Sumar()
+        {
+            resultado = numero1 + resultado;
+        }
+        public void Restar()
+        {
+            resultado = resultado - numero1;
+        }
+        public void Multiplicar()
+        {
+            resultado = resultado * numero1;
+        }
+        public void Dividir()
+        {
+            resultado = resultado / numero1;
+        }
+    }
+}
